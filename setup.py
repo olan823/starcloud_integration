d658f3d33ccfe39703a8a4a78b13175631a7ee1c
olan823 <782468678@qq.com>
2026-09-24 14:47:50 +0800

    feat: expand Chinese translations

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="starcloud_integration",
-    version="0.1.6",
+    version="0.1.18",
     description="Native ERPNext integration boundary for Starcloud",
     author="Skychip",
     packages=find_packages(),
